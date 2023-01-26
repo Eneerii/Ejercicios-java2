9c6b839eed90a3c7c4155d9a57f018f506a1f094
manana <manana@A13E12>
2023-01-26 10:47:07 +0100

    ,

--- _20_productos_nio/src/service/ProductosServiceNio.java
+++ _20_productos_nio/src/service/ProductosServiceNio.java
@@ -1,5 +1,35 @@
+
 package service;
 
-public class ProductosServiceNio {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
+public class ProductosServiceNio {
+	private Path pt=Path.of("c:\\ficheros\\productos.txt");
+//método que devuelve la lista de productos
+public List<String> listaProductos() {
+	try {
+		return Files.readAllLines(pt);
+	} catch (IOException e) {
+		e.printStackTrace();
+		return null;
+	}
 }
+
+//metodo que a partir del nombre de un producto , nos dice si esta o no almacenado 
+public boolean existeProducto(String producto) {
+	try {
+		return Files.lines(pt) //Stream<String>
+					.anyMatch(s->s.equals(producto));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+}
+
+
+
